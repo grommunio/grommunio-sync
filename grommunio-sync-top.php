ebf50b06516ac11e5a9ddaba0b0c01e06870d0ff
Michael Becker <michael.becker@grommunio.com>
2022-02-17 00:30:28 +0000

    Add redis auth to grommunio-sync-top
    References: Issue #35

--- grommunio-sync-top.php
+++ grommunio-sync-top.php
@@ -10,6 +10,8 @@
  */
 
 require_once 'vendor/autoload.php';
+if (!defined('ZPUSH_CONFIG')) define('ZPUSH_CONFIG', 'config.php');
+include_once(ZPUSH_CONFIG);
 
 /************************************************
  * MAIN
