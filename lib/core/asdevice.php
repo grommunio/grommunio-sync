79511869c1a3aa5a1d00013298689621a396e49c
Michael Becker <michael.becker@grommunio.com>
2022-07-07 16:03:55 +0000

    Undo declaration of magic properties as this breaks serialization of this data.
    References: Issue #52

--- lib/core/asdevice.php
+++ lib/core/asdevice.php
@@ -39,12 +39,6 @@ class ASDevice extends StateObject {
 		'syncfiltertype' => false,
 	];
 
-	protected $firstsynctime;
-	protected $deviceid;
-	protected $devicetype;
-	protected $deviceuser;
-	protected $domain;
-	protected $useragent;
 	protected $newdevice;
 	protected $hierarchyCache;
 	protected $ignoredMessageIds;
